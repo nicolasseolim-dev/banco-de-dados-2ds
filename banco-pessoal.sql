ee2d4d2031a6037796c16ac1beae707a396f6e8b
nicolasseolim-dev <nicolas.seolim@escola.pr.gov.br>
2026-03-05 11:21:16 -0300

    Create banco-pessoal.sql

---
+++ banco-pessoal.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Produtos (
+    lucro_do_time INT PRIMARY KEY,
+    numero_vendas VARCHAR(100) NOT NULL,
+    preco_uniforme DECIMAL(10, 2) NOT NULL,
+  
+  criticas TEXT
+);
+select * from Produtos;
